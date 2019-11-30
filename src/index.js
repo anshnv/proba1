1664e80c5fdc084fc98886d67530598226163824
unknown <anshnv@ya.ru>
2019-11-30 10:29:50 +0300

    Начало

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// JS - 
+import './js/';
+//import './pages/page.js';
+import $ from 'jquery';
+global.jQuery = $;
+global.$ = $;
+global.jQuery = global.$ = $;
+window.$ = window.jQuery = $;
+import 'jquery/dist/jquery.min.js'
+import './pages/page.js';
+// SCSS
+import './index.scss';
+// PUG 
+import './index.pug';
+
+//require('jquery-ui/ui/widgets/slider');
+//$('#id').slider();
---
+++ src/pages/page.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+   jQuery("#myid").html("Опять текст");
+
+});
+
+//var inc=0;
+//var rez=0;
+//function ClickMy(){
+	//inc=inc+1;
+	//console.log(inc);
+	//alert("Клик");
+	//return inc;
+//}
+//console.log(ClickMy());
+//console.log(inc);
+//console.log(rez);
